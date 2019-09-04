6e0d4d9e6b7577313d56eff33209e792ff0b4afd
Salma Hanafi <shanafi@uncc.edu>
2019-09-04 22:04:56 +0000

    RubyBasics1 – Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,20 @@
+# Lab 1
+# Part I
+def sum arr
+  i=0
+  sum = 0
+  for i in (0..arr.size-1)
+  sum = sum + arr[i]
+  end
+return sum
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
