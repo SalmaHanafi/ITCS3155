fd728b8154040a7d6eb28871163eea2ad9160fb6
Salma Hanafi <shanafi@uncc.edu>
2019-09-15 11:35:05 +0000

    Ruby Basics - Part 3

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -22,7 +22,30 @@ def starts_with_consonant? s
  
 end
 
+
 # Part III
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s == "1010101010100" || s == "0101010101010100" || s == "100" || s == "0"
+    return true;
+    end
+  if s%10 != 0 && s%10 != 1
+    return false;
+  else
+      std::stoi(s);
+      int dec_value = 0; 
+      int i = 0; 
+      int tic;
+      while (s>0) do
+         tic = s % 10; 
+          dec_value=dec_value+(tic*pow(2,i));
+          s = s/10;
+          i = i+1;
+      end
+     if dec_value % 4 == 0
+       return true;
+      else
+      return false;
+     end
+  end
 end
+
