8b5ddb9c0abf73c5ba472360d6a0ab2bcf7e00ef
Salma Hanafi <shanafi@uncc.edu>
2019-09-24 14:23:02 +0000

    Ruby OO Basics - Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,16 @@
 # Ruby Basics Part 3
 
 class BookInStock
+    attr_reader :isbn  #=>  def isbn; @isbn; end
+    attr_writer :isbn  #=>  def isbn=(x); @isbn = x; end
+   
+    attr_reader :price  #=>  def calories; @calories; end
+    attr_writer :price  #=>  def calories=(x); @calories = x; end
+    attr_accessor:isbn, :price #=> 
+    
+    def initialize(isbn = "", price = 0.0)
+        @isbn = isbn;
+        @price = price;
+    end
 # YOUR CODE HERE
 end
